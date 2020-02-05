a40b2383a858e262da8c0400c536ab22a4421d29
hassieswift621 <hey@hassie.ninja>
2020-02-05 10:04:46 +0000

    Setup engine VS project with MG NuGet

--- Hassie.ConcussionEngine.Engine/Properties/AssemblyInfo.cs
+++ Hassie.ConcussionEngine.Engine/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Concussion Engine")]
-[assembly: AssemblyProduct("Concussion Engine")]
+[assembly: AssemblyDescription("Concussion Engine")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Concussion Engine")]
 [assembly: AssemblyCopyright("Copyright ©2020 Hassie.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f81c3f11-3f99-4b8e-b810-1b87b9000be2")]
+[assembly: Guid("561cc640-cb1c-44f1-bc92-6db1caddbe33")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
