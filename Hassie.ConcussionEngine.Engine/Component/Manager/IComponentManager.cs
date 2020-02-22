cbd9ae5525d9066c7aba5c36379927e7aad793f2
hassieswift621 <hey@hassie.ninja>
2020-02-22 21:59:10 +0000

    Add component manager interface

---
+++ Hassie.ConcussionEngine.Engine/Component/Manager/IComponentManager.cs
@@ -0,0 +1,54 @@
+﻿using Hassie.ConcussionEngine.Engine.Component.Registry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hassie.ConcussionEngine.Engine.Component.Manager
+{
+    /// <summary>
+    /// A component manager manages components and component registries.
+    /// </summary>
+    public interface IComponentManager
+    {
+        /// <summary>
+        /// Gets a component.
+        /// </summary>
+        /// <typeparam name="C">The component type.</typeparam>
+        /// <param name="entityID">The ID of the entity.</param>
+        /// <returns>The component.</returns>
+        C GetComponent<C>(int entityID) where C : struct, IComponent;
+
+        /// <summary>
+        /// Gets a component registry.
+        /// </summary>
+        /// <typeparam name="C">The component type the registry handles.</typeparam>
+        /// <returns>The component registry.</returns>
+        IComponentRegistry GetRegistry<C>() where C : struct, IComponent;
+
+        /// <summary>
+        /// Registers a component registry for a component type.
+        /// </summary>
+        /// <typeparam name="C">The component type.</typeparam>
+        /// <typeparam name="R">The registry type.</typeparam>
+        /// <returns>The component manager instance.</returns>
+        IComponentManager RegisterType<C, R>() where C : struct, IComponent where R : IComponentRegistry<C>;
+
+        /// <summary>
+        /// Removes a component.
+        /// </summary>
+        /// <typeparam name="C">The component type.</typeparam>
+        /// <param name="entityID">The ID of the entity.</param>
+        /// <returns>The component manager instance.</returns>
+        IComponentManager Remove<C>(int entityID) where C : struct, IComponent;
+
+        /// <summary>
+        /// Sets a component.
+        /// </summary>
+        /// <typeparam name="C">The component type.</typeparam>
+        /// <param name="component">The component.</param>
+        /// <returns>The component manager instance.</returns>
+        IComponentManager Set<C>(C component) where C : struct, IComponent;
+    }
+}
