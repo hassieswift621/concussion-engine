77fe443d35005791ac03e62e455ec6189e5c6ef3
hassieswift621 <hey@hassie.ninja>
2020-02-25 21:31:00 +0000

    Implement entity manager

---
+++ Hassie.ConcussionEngine.Engine/Entity/EntityManager.cs
@@ -0,0 +1,45 @@
+﻿using Hassie.ConcussionEngine.Engine.Component.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hassie.ConcussionEngine.Engine.Entity
+{
+    /// <summary>
+    /// The entity manager handles the creation of entities and
+    /// termination via a <see cref="IComponentTerminator"/>.
+    /// </summary>
+    public class EntityManager : IEntityManager
+    {
+        // Entities created counter.
+        // This is shared across all instances of the entity manager.
+        private static int entitiesCreated;
+
+        // Component terminator.
+        private readonly IComponentTerminator componentTerminator;
+
+        /// <summary>
+        /// Constructor for the entity manager.
+        /// </summary>
+        /// <param name="terminator">A component terminator.</param>
+        public EntityManager(IComponentTerminator terminator)
+        {
+            // Store component terminator.
+            componentTerminator = terminator;
+        }
+
+        public int CreateEntity()
+        {
+            // Increment entities created.
+            return ++entitiesCreated;
+        }
+
+        public void DestroyEntity(int entityID)
+        {
+            // Terminate entity via component terminator.
+            componentTerminator.TerminateEntity(entityID);
+        }
+    }
+}
---
+++ Hassie.ConcussionEngine.Engine/Entity/IEntityManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hassie.ConcussionEngine.Engine.Entity
+{
+    /// <summary>
+    /// An entity manager handles the creation and termination of entities.
+    /// </summary>
+    public interface IEntityManager
+    {
+        /// <summary>
+        /// Creates an entity.
+        /// </summary>
+        /// <returns>The entity ID.</returns>
+        int CreateEntity();
+
+        /// <summary>
+        /// Destroys an entity.
+        /// </summary>
+        /// <param name="entityID">The ID of the entity.</param>
+        void DestroyEntity(int entityID);
+    }
+}
