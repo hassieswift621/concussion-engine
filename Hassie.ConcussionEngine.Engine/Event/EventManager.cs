05c5ed5d9b1df1d23249b06961ab99b37f4c1d57
hassieswift621 <hey@hassie.ninja>
2020-03-20 23:13:36 +0000

    Implement event manager

---
+++ Hassie.ConcussionEngine.Engine/Event/EventManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hassie.ConcussionEngine.Engine.Event
+{
+    /// <summary>
+    /// The event manager handles the events for the engine and the game.
+    /// </summary>
+    public class EventManager : IEventManager
+    {
+        // Map of event listeners.
+        // The key is the type of event the listener handles, which extend from EventArgs.
+        // The value is the event handler, stored as a generic object to allow for a generic event manager.
+        private IDictionary<Type, object> listeners;
+
+        public void Emit<T>(T data) where T : EventArgs
+        {
+            // If there aren't listeners for the type, return.
+            if (!listeners.ContainsKey(typeof(T)))
+            {
+                return;
+            }
+
+            // Emit event.
+            if (listeners.ContainsKey(typeof(T)))
+            {
+                EventHandler<T> handler = (EventHandler<T>)listeners[typeof(T)];
+                handler?.Invoke(this, data);
+            }
+        }
+
+        public void Subscribe<T>(EventHandler<T> listener) where T : EventArgs
+        {
+            // Check if are there listeners for this event.
+            // If true, subscribe.
+            // Else, add as a new event listener.
+            if (listeners.ContainsKey(typeof(T)))
+            {
+                EventHandler<T> handler = (EventHandler<T>)listeners[typeof(T)];
+                handler += listener;
+                listeners[typeof(T)] = handler;
+            }
+            else
+            {
+                listeners[typeof(T)] = listener;
+            }
+        }
+
+        public void Unsubscribe<T>(EventHandler<T> listener) where T : EventArgs
+        {
+            // If there aren't listeners for the event type, return.
+            if (!listeners.ContainsKey(typeof(T)))
+            {
+                return;
+            }
+
+            // Get event listener and unsubscribe.
+            EventHandler<T> handler = (EventHandler<T>)listeners[typeof(T)];
+            handler -= listener;
+            listeners[typeof(T)] = handler;
+        }
+    }
+}
---
+++ Hassie.ConcussionEngine.Engine/Event/IEventListener.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hassie.ConcussionEngine.Engine.Event
+{
+    /// <summary>
+    /// An event listener listens to events.
+    /// </summary>
+    /// <typeparam name="T">The type of event the listener listens to.</typeparam>
+    public interface IEventListener<T> where T : EventArgs
+    {
+        /// <summary>
+        /// On event handler.
+        /// </summary>
+        /// <param name="sender">The event emitter.</param>
+        /// <param name="args">The event args.</param>
+        void OnEvent(object sender, T args);
+    }
+}
