d0a5bb6b51f06e7acde9f0b0746d11be59fc5ac4
hassieswift621 <hey@hassie.ninja>
2020-03-22 19:51:17 +0000

    Implement keyboard input system

---
+++ Hassie.ConcussionEngine.Engine/Input/KeyboardInputEvent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hassie.ConcussionEngine.Engine.Input
+{
+    /// <summary>
+    /// Keyboard input event args.
+    /// </summary>
+    public class KeyboardInputEvent : EventArgs
+    {
+        /// <summary>
+        /// The pressed keys.
+        /// </summary>
+        public IReadOnlyList<Keys> PressedKeys;
+
+        /// <summary>
+        /// The released keys.
+        /// </summary>
+        public IReadOnlyList<Keys> ReleasedKeys;
+
+        /// <summary>
+        /// Constructor for the keyboard input event.
+        /// </summary>
+        /// <param name="pressedKeys">The pressed keys.</param>
+        /// <param name="releasedKeys">The released keys.</param>
+        public KeyboardInputEvent(IReadOnlyList<Keys> pressedKeys, IReadOnlyList<Keys> releasedKeys)
+        {
+            // Store data.
+            PressedKeys = pressedKeys;
+            ReleasedKeys = releasedKeys;
+        }
+    }
+}
---
+++ Hassie.ConcussionEngine.Engine/Input/KeyboardInputSystem.cs
@@ -0,0 +1,71 @@
+﻿using Hassie.ConcussionEngine.Engine.Update;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hassie.ConcussionEngine.Engine.Input
+{
+    /// <summary>
+    /// The keyboard input system handles keyboard input.
+    /// </summary>
+    public class KeyboardInputSystem : AbstractUpdateSystem
+    {
+        // Previous keyboard state.
+        private IList<Keys> state;
+
+        /// <summary>
+        /// Constructor for the keyboard input system.
+        /// </summary>
+        public KeyboardInputSystem()
+        {
+            // Initialise state to empty list.
+            state = new List<Keys>();
+        }
+
+        public override void Destroy()
+        {
+            return;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // Get keyboard state.
+            IList<Keys> keys = Keyboard.GetState().GetPressedKeys().ToList();
+
+            // Initialise lists to hold pressed and released keys.
+            List<Keys> pressedKeys = new List<Keys>();
+            List<Keys> releasedKeys = new List<Keys>();
+
+            // Run through new keyboard state and compare with old state.
+            foreach (Keys key in keys)
+            {
+                // If key is not in the previous state, this is a newly pressed key.
+                if (!state.Contains(key))
+                {
+                    pressedKeys.Add(key);
+                }
+                // Else, remove key from previous state.
+                else
+                {
+                    state.Remove(key);
+                }
+            }
+
+            // At this point, the previous state will contain the released keys, if any.
+            releasedKeys = state.ToList();
+
+            // Store new state.
+            state = keys;
+
+            // If pressed keys > 0 or released keys > 0, emit event.
+            if (pressedKeys.Count > 0 || releasedKeys.Count > 0)
+            {
+                eventManager.Emit(new KeyboardInputEvent(pressedKeys, releasedKeys));
+            }
+        }
+    }
+}
