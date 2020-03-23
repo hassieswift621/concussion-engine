c213e04a9533d74caee567d731d5bceaaf159fe6
hassieswift621 <hey@hassie.ninja>
2020-03-23 20:58:03 +0000

    Bare running game

--- Hassie.ConcussionEngine.Pong/Pong.cs
+++ Hassie.ConcussionEngine.Pong/Pong.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hassie.ConcussionEngine.Engine.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Hassie.ConcussionEngine.Pong
 {
-    public class Pong : Engine.
+    public class Pong : Engine.Core.Engine
     {
     }
 }
---
+++ Hassie.ConcussionEngine.Pong/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hassie.ConcussionEngine.Pong
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Pong())
+            {
+                game.Run();
+            }
+        }
+    }
+}
