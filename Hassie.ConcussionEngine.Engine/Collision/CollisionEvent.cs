6bdd50710c695cb2bf2f7e861abef44ad36db859
hassieswift621 <hey@hassie.ninja>
2020-03-23 14:36:55 +0000

    Implement hitbox collision system

---
+++ Hassie.ConcussionEngine.Engine/Collision/CollisionEvent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hassie.ConcussionEngine.Engine.Collision
+{
+    /// <summary>
+    /// Collisio event args.
+    /// </summary>
+    public class CollisionEvent : EventArgs
+    {
+        /// <summary>
+        /// The ID of the colliding entity.
+        /// </summary>
+        public int Collider { get; }
+
+        /// <summary>
+        /// The ID of the collidable entity.
+        /// </summary>
+        public int Collidable { get; }
+
+        /// <summary>
+        /// The ID of the world in which this collision event occurred.
+        /// </summary>
+        public int WorldID { get; }
+
+        /// <summary>
+        /// Constructor for the collision event.
+        /// </summary>
+        /// <param name="collider">The ID of the colliding entity.</param>
+        /// <param name="collidable">The ID of the collidable entity.</param>
+        /// <param name="worldID">The ID of the world in which this collision event occurred.</param>
+        public CollisionEvent(int collider, int collidable, int worldID)
+        {
+            // Store data.
+            Collider = collider;
+            Collidable = collidable;
+            WorldID = worldID;
+        }
+    }
+}
---
+++ Hassie.ConcussionEngine.Engine/Collision/HitboxCollisionSystem.cs
@@ -0,0 +1,89 @@
+﻿using Hassie.ConcussionEngine.Engine.Component.Data;
+using Hassie.ConcussionEngine.Engine.Component.Registry;
+using Hassie.ConcussionEngine.Engine.Update;
+using Hassie.ConcussionEngine.Engine.World;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hassie.ConcussionEngine.Engine.Collision
+{
+    /// <summary>
+    /// The hitbox collision system calculates collisions using the hitbox collision method
+    /// and emits events for collision events.
+    /// </summary>
+    public class HitboxCollisionSystem : AbstractUpdateSystem
+    {
+        public override void Destroy()
+        {
+            // Do nothing.
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // Var to store collision event data.
+            IList<CollisionEvent> collisionEvents = new List<CollisionEvent>();
+
+            // Get running worlds.
+            IReadOnlyList<IWorld> worlds = worldManager.GetWorldsInState(WorldState.Running);
+
+            // Run through running worlds and check for collisions.
+            foreach (IWorld world in worlds)
+            {
+                // Get required component registries.
+                IComponentRegistry<CollidableComponent> collidableComponents =
+                    (IComponentRegistry<CollidableComponent>)world.ComponentManager.GetRegistry<CollidableComponent>();
+                IComponentRegistry<HitboxCollisionComponent> hitboxCollisionComponents =
+                    (IComponentRegistry<HitboxCollisionComponent>)world.ComponentManager.GetRegistry<HitboxCollisionComponent>();
+                IComponentRegistry<PositionComponent> positionComponents =
+                    (IComponentRegistry<PositionComponent>)world.ComponentManager.GetRegistry<PositionComponent>();
+                IComponentRegistry<TransformComponent> transformComponents =
+                    (IComponentRegistry<TransformComponent>)world.ComponentManager.GetRegistry<TransformComponent>();
+
+                // Iterate hitbox collision components.
+                for (int i = 0; i < hitboxCollisionComponents.Count; i++)
+                {
+                    // Get components.
+                    HitboxCollisionComponent hitbox = hitboxCollisionComponents[i];
+                    PositionComponent position = positionComponents.Get(hitbox.EntityID);
+                    TransformComponent transform = transformComponents.Get(hitbox.EntityID);
+
+                    // Iterate collidable components and calculate collision.
+                    for (int j = 0; j < collidableComponents.Count; j++)
+                    {
+                        // Get components for collidable entity.
+                        CollidableComponent collidable = collidableComponents[j];
+                        PositionComponent collidablePosition = positionComponents.Get(collidable.EntityID);
+                        TransformComponent collidableTransform = transformComponents.Get(collidable.EntityID);
+
+                        // Create hitboxes.
+                        Rectangle entityA = new Rectangle((int)position.X, (int)position.Y, transform.Width, transform.Height);
+                        Rectangle entityB = new Rectangle((int)collidablePosition.X, (int)collidablePosition.Y,
+                            collidableTransform.Width, collidableTransform.Height);
+
+                        // Perform collision detection.
+                        if (entityA.Intersects(entityB))
+                        {
+                            // Entities collided.
+                            collisionEvents.Add(new CollisionEvent(hitbox.EntityID, collidable.EntityID, world.ID));
+                        }
+                    }
+                }
+            }
+
+            // An entity can be both a collider and a collidable, so filter out any events where this is true.
+            // We could check this before performing the collision detection but we want to avoid
+            // branch predictions in the hot path as much as possible.
+            collisionEvents = collisionEvents.Where(x => x.Collidable != x.Collider).ToList();
+
+            // Emit events.
+            foreach (CollisionEvent collisionEvent in collisionEvents)
+            {
+                eventManager.Emit(collisionEvent);
+            }
+        }
+    }
+}
