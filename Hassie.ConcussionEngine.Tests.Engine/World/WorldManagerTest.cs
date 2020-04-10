384374d16ce615599ae120ae38b96879b4aacd44
hassieswift621 <hey@hassie.ninja>
2020-04-10 21:35:37 +0100

    More world management tests

--- Hassie.ConcussionEngine.Tests.Engine/World/WorldManagerTest.cs
+++ Hassie.ConcussionEngine.Tests.Engine/World/WorldManagerTest.cs
@@ -72,5 +72,79 @@ namespace Hassie.ConcussionEngine.Tests.Engine.World
             // Assert.
             Assert.IsTrue(worlds.Count == 2 && worlds.Contains(world1) && worlds.Contains(world2));
         }
+
+        /// <summary>
+        /// Tests the pausing of a world.
+        /// </summary>
+        [Test]
+        public void PauseWorld()
+        {
+            // Arrange.
+            IWorldManager worldManager = new WorldManager();
+            AbstractWorld world = new MockWorld(1, null, null);
+            worldManager.AddWorld(world);
+
+            // Act.
+            worldManager.PauseWorld(1);
+
+            // Assert.
+            Assert.IsTrue(world.State == WorldState.Paused);
+        }
+
+        /// <summary>
+        /// Tests the running of a world.
+        /// </summary>
+        [Test]
+        public void RunWorld()
+        {
+            // Arrange.
+            IWorldManager worldManager = new WorldManager();
+            AbstractWorld world = new MockWorld(1, null, null);
+            worldManager.AddWorld(world);
+
+            // Act.
+            worldManager.RunWorld(1);
+
+            // Assert.
+            Assert.IsTrue(world.State == WorldState.Running);
+        }
+
+        /// <summary>
+        /// Tests the stopping of a world.
+        /// </summary>
+        [Test]
+        public void StopWorld()
+        {
+            // Arrange.
+            IWorldManager worldManager = new WorldManager();
+            AbstractWorld world = new MockWorld(1, null, null);
+            worldManager.AddWorld(world);
+            worldManager.RunWorld(1);
+
+            // Act.
+            worldManager.StopWorld(1, false);
+
+            // Assert.
+            Assert.IsTrue(world.State == WorldState.Stopped);
+        }
+
+        /// <summary>
+        /// Tests the resetting of a world.
+        /// </summary>
+        [Test]
+        public void StopWorld_Reset()
+        {
+            // Arrange.
+            IWorldManager worldManager = new WorldManager();
+            AbstractWorld world = new MockWorld(1, null, null);
+            worldManager.AddWorld(world);
+            worldManager.RunWorld(1);
+
+            // Act.
+            worldManager.StopWorld(1, true);
+
+            // Assert.
+            Assert.IsTrue(world.State == WorldState.None);
+        }
     }
 }
